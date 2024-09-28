9f28ba8b625180180ce81300b7e2d5f89a378c74
Dave Nicholson <me@davenicholson.xyz>
2024-09-28 07:30:34 +0100

    remove var

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', async function() {
   try {
-    const response = await fetch("http://localhost:2388")
+    await fetch("http://localhost:2388")
     window.location.href = "found.html";
   } catch (e) {
     window.location.href = "not_found.html";
