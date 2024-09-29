492b6886a19ba8deaa48c09e342d83540b140edf
Dave Nicholson <me@davenicholson.xyz>
2024-09-29 13:54:45 +0100

    wm dependant wallpaper script

--- examples/setwallpaper.sh
+++ examples/setwallpaper.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# Sample script to set wallpaper on linux or mac
-
 if [ -n "$1" ]; then
   IMG=$1
 else
@@ -15,30 +13,33 @@ if ! [[ $EXT =~ ^(jpg|jpeg|png)$ ]]; then
 fi
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  # the following are some examples of setting wallpaper on various WMs
-
-  # Wayland
-  swww img $IMG
-
-  ## X11
-  #feh --bg-fill "$IMG"
-
-  ## Gnome / Ubuntu
-  #gsettings set org.gnome.desktop.background picture-uri file://"$IMG"
-  #gsettings set org.gnome.desktop.background picture-uri-dark file://"$IMG"
-
-  ## Plasma
-  # dbus-send --session --dest=org.kde.plasmashell --type=method_call /PlasmaShell org.kde.PlasmaShell.evaluateScript string:"
-  # var allDesktops = desktops();
-  # for (i = 0; i < allDesktops.length; i++) {
-  #     d = allDesktops[i];
-  #     d.wallpaperPlugin = 'org.kde.image';
-  #     d.currentConfigGroup = Array('Wallpaper', 'org.kde.image', 'General');
-  #     d.writeConfig('Image', 'file://$IMG');
-  # }
-  # "
+  case "$DESKTOP_SESSION" in
+  plasma)
+    dbus-send --session --dest=org.kde.plasmashell --type=method_call /PlasmaShell org.kde.PlasmaShell.evaluateScript string:"
+      var allDesktops = desktops();
+      for (i = 0; i < allDesktops.length; i++) {
+          d = allDesktops[i];
+          d.wallpaperPlugin = 'org.kde.image';
+          d.currentConfigGroup = Array('Wallpaper', 'org.kde.image', 'General');
+          d.writeConfig('Image', 'file://$IMG');
+      }
+      "
+    ;;
+  ubuntu | gnome)
+    gsettings set org.gnome.desktop.background pickture-uri file://"$IMG"
+    gsettings set org.gnome.desktop.background picture-uri-dark file://"$IMG"
+    ;;
+  hyprland)
+    swww img "$IMG"
+    ;;
+  i3)
+    feh --bg-fill "$IMG"
+    ;;
+  *)
+    echo "Unknown window manager"
+    ;;
+  esac
 
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-  # Change wallpaper in OSX.
   osascript -e "tell application \"Finder\" to set desktop picture to POSIX file \"$IMG\""
 fi
