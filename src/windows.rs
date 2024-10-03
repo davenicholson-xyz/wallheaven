fef55a3affd83092f5e07c13ecb49aed12a3cdfb
Dave Nicholson <me@davenicholson.xyz>
2024-10-03 23:49:32 +0100

    windows setting

--- src/windows.rs
+++ src/windows.rs
@@ -1,6 +1,31 @@
 use anyhow::Result;
+use std::ffi::OsStr;
+use std::os::windows::ffi::OsStrExt;
+use std::path::Path;
+use windows::Win32::UI::WindowsAndMessaging::SystemParametersInfoW;
+use windows::Win32::UI::WindowsAndMessaging::{
+    SPIF_SENDCHANGE, SPIF_UPDATEINIFILE, SPI_SETDESKWALLPAPER,
+};
 
-pub fn set_wallpaper(filepath: &str) -> Result<()> {
-    println!("set wallpaper on windows");
-    Ok(())
+fn set_wallpaper(filepath: &str) -> Result<()> {
+    let path: Vec<u16> = OsStr::new(image_path)
+        .encode_wide()
+        .chain(Some(0))
+        .collect();
+
+    let result = unsafe {
+        SystemParametersInfoW(
+            SPI_SETDESKWALLPAPER,
+            0,
+            path.as_ptr() as *mut _,
+            SPIF_UPDATEINIFILE | SPIF_SENDCHANGE,
+        )
+    };
+
+    if result.as_bool() {
+        Ok(())
+    } else {
+        println!("{}", filepath);
+        Ok(())
+    }
 }
