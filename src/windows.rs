d6364198c2e09713e2b85cfc3cb730bd0564c2af
Dave Nicholson <me@davenicholson.xyz>
2024-10-04 08:33:34 +0100

    added windows back

--- src/windows.rs
+++ src/windows.rs
@@ -1,6 +1,36 @@
 use anyhomw::Result;
+use std::ffi::OsStr;
+use std::io;
+use std::iter;
+use std::mem;
+use std::os::windows::ffi::OsStrExt;
+use winapi::ctypes::c_void;
+use winapi::um::winuser::SystemParametersInfoW;
+use winapi::um::winuser::SPIF_SENDCHANGE;
+use winapi::um::winuser::SPIF_UPDATEINIFILE;
+use winapi::um::winuser::SPI_GETDESKWALLPAPER;
+use winapi::um::winuser::SPI_SETDESKWALLPAPER;
+use winreg::enums::*;
+use winreg::RegKey;
 
 pub fn set_wallpaper(filepath: &str) -> Result<()> {
-    println!("windowz");
-    Ok(())
+    unsafe {
+        let path = OsStr::new(path)
+            .encode_wide()
+            // append null byte
+            .chain(iter::once(0))
+            .collect::<Vec<u16>>();
+        let successful = SystemParametersInfoW(
+            SPI_SETDESKWALLPAPER,
+            0,
+            path.as_ptr() as *mut c_void,
+            SPIF_UPDATEINIFILE | SPIF_SENDCHANGE,
+        ) == 1;
+
+        if successful {
+            Ok(())
+        } else {
+            println!("{}", filepath);
+        }
+    }
 }
